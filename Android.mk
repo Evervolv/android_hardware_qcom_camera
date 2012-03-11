23993eb0f79621e5851eee218b41da2ff222470e
andrew.boren <andrew.boren@gmail.com>
2012-03-11 02:19:37 -0700

    Fix missing ifdef.
    Change-Id: Idc16b9f4877cb1cfb7b4aea486921792245ff9b4

--- Android.mk
+++ Android.mk
@@ -1,21 +1,23 @@
-LOCAL_PATH := $(call my-dir)
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+    LOCAL_PATH := $(call my-dir)
 
-include $(call all-subdir-makefiles)
+    include $(call all-subdir-makefiles)
 
-include $(CLEAR_VARS)
+    include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS    := optional
-LOCAL_MODULE_PATH    := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE         := camera.$(TARGET_BOARD_PLATFORM)
+    LOCAL_MODULE_TAGS    := optional
+    LOCAL_MODULE_PATH    := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+    LOCAL_MODULE         := camera.$(TARGET_BOARD_PLATFORM)
 
-LOCAL_SRC_FILES      := cameraHal.cpp
+    LOCAL_SRC_FILES      := cameraHal.cpp
 
-TARGET_GLOBAL_LD_DIRS  += -L$(LOCAL_PATH)
-LOCAL_SHARED_LIBRARIES := liblog libdl libutils libcamera_client libbinder libcutils libhardware
-LOCAL_SHARED_LIBRARIES += libcamera
-LOCAL_C_INCLUDES       := frameworks/base/services/ frameworks/base/include
-LOCAL_C_INCLUDES       += hardware/libhardware/include/ hardware/libhardware/modules/gralloc/
+    TARGET_GLOBAL_LD_DIRS  += -L$(LOCAL_PATH)
+    LOCAL_SHARED_LIBRARIES := liblog libdl libutils libcamera_client libbinder libcutils libhardware
+    LOCAL_SHARED_LIBRARIES += libcamera
+    LOCAL_C_INCLUDES       := frameworks/base/services/ frameworks/base/include
+    LOCAL_C_INCLUDES       += hardware/libhardware/include/ hardware/libhardware/modules/gralloc/
 
-LOCAL_PRELINK_MODULE := false
+    LOCAL_PRELINK_MODULE := false
 
-include $(BUILD_SHARED_LIBRARY)
+    include $(BUILD_SHARED_LIBRARY)
+endif
