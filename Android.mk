81569999830814cd4e912028a4fbb365b41767ec
Serban Zodian <giant@kappa.ro>
2012-07-18 12:06:05 +0300

    fix c/p missbehaving
    Change-Id: Icdb1a6cf1f0bf7f93f58bc9db954322b370b5a3c

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates//)
+$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/)
 $(shell touch $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/export_includes)
 
 LOCAL_PATH := $(call my-dir)
