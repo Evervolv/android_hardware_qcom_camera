5bec98736070b410c2a2f7d519d878dec26b2241
Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>
2014-10-08 22:17:37 +0200

    Remove unused variable
    Change-Id: Ie81e0f3b4535d53f9cf0b3eaca4d8f6681feab6f
    Signed-off-by: Bernhard Rosenkraenzer <Bernhard.Rosenkranzer@linaro.org>

--- QCamera2/HAL3/QCamera3Channel.cpp
+++ QCamera2/HAL3/QCamera3Channel.cpp
@@ -47,8 +47,6 @@ using namespace android;
 namespace qcamera {
 static const char ExifAsciiPrefix[] =
     { 0x41, 0x53, 0x43, 0x49, 0x49, 0x0, 0x0, 0x0 };          // "ASCII\0\0\0"
-static const char ExifUndefinedPrefix[] =
-    { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };   // "\0\0\0\0\0\0\0\0"
 
 #define GPS_PROCESSING_METHOD_SIZE       101
 #define EXIF_ASCII_PREFIX_SIZE           8   //(sizeof(ExifAsciiPrefix))
