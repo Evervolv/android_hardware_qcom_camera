6cf631c577546802df00d1d151a24d7bcfadb92d
Shuzhen Wang <shuzhenwang@google.com>
2016-06-03 15:06:16 -0700

    QCamera2: HAL3: Add post stats digital gain metadata
    In ISP, there are multiple modules that can apply digital gain. Add a
    separate POST_STATS_SENSITIVITY to report the digital gain applied after
    stats collection.
    
    Bug: 29103344
    Change-Id: Icd4fb0b5735a39c930a057a74dafc66a748b2d2d

--- msm8998/QCamera2/HAL3/QCamera3HWI.cpp
+++ msm8998/QCamera2/HAL3/QCamera3HWI.cpp
@@ -6077,10 +6077,14 @@ QCamera3HardwareInterface::translateFromHalMetadata(
     }
 
 #ifndef USE_HAL_3_3
+    int32_t fwk_ispSensitivity = 100;
     IF_META_AVAILABLE(int32_t, ispSensitivity, CAM_INTF_META_ISP_SENSITIVITY, metadata) {
-        int32_t fwk_ispSensitivity = (int32_t) *ispSensitivity;
-        camMetadata.update(ANDROID_CONTROL_POST_RAW_SENSITIVITY_BOOST, &fwk_ispSensitivity, 1);
+        fwk_ispSensitivity = (int32_t) *ispSensitivity;
     }
+    IF_META_AVAILABLE(float, postStatsSensitivity, CAM_INTF_META_ISP_POST_STATS_SENSITIVITY, metadata) {
+        fwk_ispSensitivity = (int32_t) (*postStatsSensitivity * fwk_ispSensitivity);
+    }
+    camMetadata.update(ANDROID_CONTROL_POST_RAW_SENSITIVITY_BOOST, &fwk_ispSensitivity, 1);
 #endif
 
     IF_META_AVAILABLE(uint32_t, shadingMode, CAM_INTF_META_SHADING_MODE, metadata) {
