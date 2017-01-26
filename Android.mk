cc4b48fc02ae0c764247170f14f211ed0f7c32c5
Mark Urbanus <urbanus@google.com>
2017-01-26 11:29:59 -0800

    Only build the msm8996 camera HAL if we're not building the VR impl.
    Introduce a makefile conditional that prevents the generic msm8996 camera HAL
    being pulled in if we're building for a VR product.
    
    Test: none
    Bug: none
    Change-Id: Ic35f9ae25248ba29b0eb234d7e916dd397d13523

--- Android.mk
+++ Android.mk
@@ -2,8 +2,10 @@
 ifneq ($(TARGET_BOARD_AUTO),true)
   ifneq ($(strip $(USE_CAMERA_STUB)),true)
     ifneq ($(BUILD_TINY_ANDROID),true)
-      ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
-        include $(addsuffix /Android.mk, $(addprefix $(call my-dir)/, mm-image-codec QCamera2))
+      ifneq ($(USE_VR_CAMERA_HAL), true)
+        ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
+          include $(addsuffix /Android.mk, $(addprefix $(call my-dir)/, mm-image-codec QCamera2))
+        endif
       endif
       ifneq ($(filter msm8998,$(TARGET_BOARD_PLATFORM)),)
         include $(call all-makefiles-under,$(call my-dir)/msm8998)
