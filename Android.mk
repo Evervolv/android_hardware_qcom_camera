6354348fd5a1926a8b83c041bf8df6b918f6c33a
Zhijun He <zhijunhe@google.com>
2017-03-08 10:27:04 -0800

    Update makefile for marlin camera HAL relocation
    Bug: 34165060
    Change-Id: I46ce868a3f4f519c35a4802e896909de3ec1d5d4

--- Android.mk
+++ Android.mk
@@ -3,9 +3,6 @@ ifneq ($(TARGET_BOARD_AUTO),true)
   ifneq ($(strip $(USE_CAMERA_STUB)),true)
     ifneq ($(BUILD_TINY_ANDROID),true)
       ifneq ($(USE_VR_CAMERA_HAL), true)
-        ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
-          include $(addsuffix /Android.mk, $(addprefix $(call my-dir)/, mm-image-codec QCamera2))
-        endif
         ifneq ($(filter msm8998,$(TARGET_BOARD_PLATFORM)),)
           include $(call all-makefiles-under,$(call my-dir)/msm8998)
         endif
