LineageOS/android_device_google_marlin

QCamera2: Replace ATRACE_INT with ATRACE_ASYNC_BEGIN/END
Test: Collect and observe systrace
Change-Id: Ibae3fbe8f46c0e7d5bac606fdca4c146f38e153e

camera/QCamera2/HAL3/QCamera3Channel.cpp
@@ -3117,7 +3117,6 @@ void QCamera3PicChannel::jpegEvtHandle(jpeg_job_status_t status,
     camera3_stream_buffer_t result;
     camera3_jpeg_blob_t jpegHeader;
 
-    KPI_ATRACE_INT("SNAPSHOT", 0);
     QCamera3PicChannel *obj = (QCamera3PicChannel *)userdata;
     if (obj) {
         //Construct payload for process_capture_result. Call mChannelCb
@@ -3246,6 +3245,7 @@ void QCamera3PicChannel::jpegEvtHandle(jpeg_job_status_t status,
 
             LOGI("Issue Jpeg Callback frameNumber = %d status = %d",
                     resultFrameNumber, resultStatus);
+            KPI_ATRACE_ASYNC_END("SNAPSHOT", resultFrameNumber);
             if (obj->mChannelCB) {
                 obj->mChannelCB(NULL,
                         &result,

camera/QCamera2/HAL3/QCamera3HWI.cpp
@@ -4121,7 +4121,7 @@ no_error:
     }
 
     if (blob_request) {
-        KPI_ATRACE_INT("SNAPSHOT", 1);
+        KPI_ATRACE_ASYNC_BEGIN("SNAPSHOT", frameNumber);
     }
     if (blob_request && mRawDumpChannel) {
         LOGD("Trigger Raw based on blob request if Raw dump is enabled");

camera/QCamera2/HAL3/QCamera3PostProc.cpp
@@ -601,7 +601,7 @@ int32_t QCamera3PostProcessor::processData(mm_camera_super_buf_t *input,
 int32_t QCamera3PostProcessor::processData(qcamera_fwk_input_pp_data_t *frame)
 {
     if (frame->reproc_config.reprocess_type != REPROCESS_TYPE_NONE) {
-        KPI_ATRACE_INT("Camera:Reprocess", 1);
+        KPI_ATRACE_ASYNC_BEGIN("Camera:Reprocess", frame->frameNumber);
         pthread_mutex_lock(&mReprocJobLock);
         // enqueu to post proc input queue
         m_inputFWKPPQ.enqueue((void *)frame);
@@ -706,7 +706,6 @@ int32_t QCamera3PostProcessor::processJpegSettingData(
 int32_t QCamera3PostProcessor::processPPData(mm_camera_super_buf_t *frame)
 {
     qcamera_hal3_pp_data_t *job = (qcamera_hal3_pp_data_t *)m_ongoingPPQ.dequeue();
-    KPI_ATRACE_INT("Camera:Reprocess", 0);
     if (job == NULL || ((NULL == job->src_frame) && (NULL == job->fwk_src_frame))) {
         LOGE("Cannot find reprocess job");
         return BAD_VALUE;
@@ -730,6 +729,7 @@ int32_t QCamera3PostProcessor::processPPData(mm_camera_super_buf_t *frame)
     if (NULL == job->fwk_src_frame) {
         jpeg_job->metadata = job->metadata;
     } else {
+        KPI_ATRACE_ASYNC_END("Camera:Reprocess", job->fwk_src_frame->frameNumber);
         jpeg_job->metadata =
                 (metadata_buffer_t *) job->fwk_src_frame->metadata_buffer.buffer;
         jpeg_job->fwk_src_buffer = job->fwk_src_frame;

Evervolv/android_hardware_qcom_camera

msm8998/QCamera2/HAL3/QCamera3Channel.cpp
@@ -3261,7 +3261,6 @@ void QCamera3PicChannel::jpegEvtHandle(jpeg_job_status_t status,
     camera3_stream_buffer_t result;
     camera3_jpeg_blob_t jpegHeader;
 
-    KPI_ATRACE_CAMSCOPE_INT("SNAPSHOT", CAMSCOPE_HAL3_SNAPSHOT, 0);
     QCamera3PicChannel *obj = (QCamera3PicChannel *)userdata;
     if (obj) {
         //Construct payload for process_capture_result. Call mChannelCb
@@ -3369,6 +3368,7 @@ void QCamera3PicChannel::jpegEvtHandle(jpeg_job_status_t status,
 
             LOGI("Issue Jpeg Callback frameNumber = %d status = %d",
                     resultFrameNumber, resultStatus);
+            ATRACE_ASYNC_END("SNAPSHOT", resultFrameNumber);
             if (obj->mChannelCB) {
                 obj->mChannelCB(NULL,
                         &result,

msm8998/QCamera2/HAL3/QCamera3HWI.cpp
@@ -5359,7 +5359,7 @@ no_error:
     }
 
     if (blob_request) {
-        KPI_ATRACE_CAMSCOPE_INT("SNAPSHOT", CAMSCOPE_HAL3_SNAPSHOT, 1);
+        ATRACE_ASYNC_BEGIN("SNAPSHOT", frameNumber);
         mPerfLockMgr.acquirePerfLock(PERF_LOCK_TAKE_SNAPSHOT);
     }
     if (blob_request && mRawDumpChannel) {

msm8998/QCamera2/HAL3/QCamera3PostProc.cpp
@@ -653,7 +653,7 @@ bool QCamera3PostProcessor::needsReprocess(qcamera_fwk_input_pp_data_t *frame)
 int32_t QCamera3PostProcessor::processData(qcamera_fwk_input_pp_data_t *frame)
 {
     if (needsReprocess(frame)) {
-        ATRACE_INT("Camera:Reprocess", 1);
+        ATRACE_ASYNC_BEGIN("Camera:Reprocess", frame->frameNumber);
         LOGH("scheduling framework reprocess");
         pthread_mutex_lock(&mReprocJobLock);
         // enqueu to post proc input queue
@@ -759,7 +759,6 @@ int32_t QCamera3PostProcessor::processJpegSettingData(
 int32_t QCamera3PostProcessor::processPPData(mm_camera_super_buf_t *frame)
 {
     qcamera_hal3_pp_data_t *job = (qcamera_hal3_pp_data_t *)m_ongoingPPQ.dequeue();
-    ATRACE_INT("Camera:Reprocess", 0);
     if (job == NULL || ((NULL == job->src_frame) && (NULL == job->fwk_src_frame))) {
         LOGE("Cannot find reprocess job");
         return BAD_VALUE;
@@ -783,6 +782,7 @@ int32_t QCamera3PostProcessor::processPPData(mm_camera_super_buf_t *frame)
     if (NULL == job->fwk_src_frame) {
         jpeg_job->metadata = job->metadata;
     } else {
+        ATRACE_ASYNC_END("Camera:Reprocess", job->fwk_src_frame->frameNumber);
         jpeg_job->metadata =
                 (metadata_buffer_t *) job->fwk_src_frame->metadata_buffer.buffer;
         jpeg_job->fwk_src_buffer = job->fwk_src_frame;