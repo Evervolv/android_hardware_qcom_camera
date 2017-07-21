181c57be5bf99c19ce31e51e56505f6ec7f3947d
Shuzhen Wang <shuzhenwang@google.com>
2017-07-21 11:39:44 -0700

    QCamera2: HAL3: Support AF_STATE in both partial result and final result
    Currently AF_STATE during AF_TRIGGER is sent 3 frames early
    (apply_delay 2, and report_delay 1). Without AF_TRIGGER, the AF_STATE
    is reported at the right time: after AF stats for the current readout
    frame are analyzed.
    
    In the case where AF_TRIGGER results in a state transition, send the new
    state in partial result, and use last know AF state for all preceding
    results. This shift applies to both TRIGGER_START and TRIGGER_CANCEL.
    
    Also move af_mode back to partial result.
    
    Test: Camera CTS, and AF_STATE during HDR+ burst.
    Bug: 63766738
    Change-Id: I968ca06d6909996a0b77d3b247ed0722ab92bb6a

--- msm8998/QCamera2/stack/common/cam_types.h
+++ msm8998/QCamera2/stack/common/cam_types.h
@@ -2541,7 +2541,8 @@ typedef enum {
     CAM_INTF_META_STATS_HISTOGRAM_BINS,
     /* AF regions condidence */
     CAM_INTF_META_AF_REGIONS_CONFIDENCE,
-
+    /* Early AF state due to trigger */
+    CAM_INTF_META_EARLY_AF_STATE,
     CAM_INTF_PARM_MAX
 } cam_intf_parm_type_t;
 
