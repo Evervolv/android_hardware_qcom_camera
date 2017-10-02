6ed36ca0e31f927e8ae93c51ef8c0733a9e2aa05
Chien-Yu Chen <cychen@google.com>
2017-10-02 11:22:17 -0700

    QCamera3: Fix Easel HDR+ mode after flush
    Reset mFirstPreviewIntentSeen in flush if HAL intends to restart the
    channel after flush. Without this, Easel HDR+ mode will not be
    enabled after flush, unless a new session is configured.
    
    Create a HDR+ client listener handling thread to avoid handling
    HDR+ client callbacks in HAL in the callback thread, which may lead
    to deadlocks.
    
    Test: Camera CTS
    Bug: 67112292
    Bug: 65448794
    Change-Id: I3f1ad5d6a610b8e0f21e476c49906b461d8c36b2

--- msm8998/QCamera2/HAL3/QCamera3HWI.cpp
+++ msm8998/QCamera2/HAL3/QCamera3HWI.cpp
@@ -919,6 +919,14 @@ int QCamera3HardwareInterface::openCamera(struct hw_device_t **hw_device)
                 mEaselFwUpdated = false;
             }
             gActiveEaselClient++;
+
+            mQCamera3HdrPlusListenerThread = new QCamera3HdrPlusListenerThread(this);
+            rc = mQCamera3HdrPlusListenerThread->run("QCamera3HdrPlusListenerThread");
+            if (rc != OK) {
+                ALOGE("%s: Starting HDR+ client listener thread failed: %s (%d)", __FUNCTION__,
+                        strerror(-rc), rc);
+                return rc;
+            }
         }
     }
 
@@ -941,6 +949,10 @@ int QCamera3HardwareInterface::openCamera(struct hw_device_t **hw_device)
                 }
                 gActiveEaselClient--;
             }
+
+            mQCamera3HdrPlusListenerThread->requestExit();
+            mQCamera3HdrPlusListenerThread->join();
+            mQCamera3HdrPlusListenerThread = nullptr;
         }
     }
 
@@ -1139,6 +1151,10 @@ int QCamera3HardwareInterface::closeCamera()
             }
             gActiveEaselClient--;
         }
+
+        mQCamera3HdrPlusListenerThread->requestExit();
+        mQCamera3HdrPlusListenerThread->join();
+        mQCamera3HdrPlusListenerThread = nullptr;
     }
 
     return rc;
@@ -6440,6 +6456,7 @@ int QCamera3HardwareInterface::flush(bool restartChannels, bool stopChannelImmed
                 return rc;
             }
         }
+        mFirstPreviewIntentSeen = false;
     }
     pthread_mutex_unlock(&mMutex);
 
@@ -15192,7 +15209,7 @@ status_t QCamera3HardwareInterface::openHdrPlusClientAsyncLocked()
         return OK;
     }
 
-    status_t res = gEaselManagerClient->openHdrPlusClientAsync(this);
+    status_t res = gEaselManagerClient->openHdrPlusClientAsync(mQCamera3HdrPlusListenerThread.get());
     if (res != OK) {
         ALOGE("%s: Opening HDR+ client asynchronously failed: %s (%d)", __FUNCTION__,
                 strerror(-res), res);
@@ -15768,25 +15785,20 @@ void QCamera3HardwareInterface::onFailedCaptureResult(pbcamera::CaptureResult *f
             streamBuffer.acquire_fence = -1;
             streamBuffer.release_fence = -1;
 
-            streamBuffers.push_back(streamBuffer);
-
             // Send out error buffer event.
             camera3_notify_msg_t notify_msg = {};
             notify_msg.type = CAMERA3_MSG_ERROR;
             notify_msg.message.error.frame_number = pendingBuffers->frame_number;
-            notify_msg.message.error.error_code = CAMERA3_MSG_ERROR_BUFFER;
+            notify_msg.message.error.error_code = CAMERA3_MSG_ERROR_REQUEST;
             notify_msg.message.error.error_stream = buffer.stream;
 
             orchestrateNotify(&notify_msg);
+            mOutputBufferDispatcher.markBufferReady(pendingBuffers->frame_number, streamBuffer);
         }
 
-        camera3_capture_result_t result = {};
-        result.frame_number = pendingBuffers->frame_number;
-        result.num_output_buffers = streamBuffers.size();
-        result.output_buffers = &streamBuffers[0];
+        mShutterDispatcher.clear(pendingBuffers->frame_number);
 
-        // Send out result with buffer errors.
-        orchestrateResult(&result);
+
 
         // Remove pending buffers.
         mPendingBuffersMap.mPendingBuffersInRequest.erase(pendingBuffers);
@@ -15804,7 +15816,6 @@ void QCamera3HardwareInterface::onFailedCaptureResult(pbcamera::CaptureResult *f
 
     pthread_mutex_unlock(&mMutex);
 }
-
 
 ShutterDispatcher::ShutterDispatcher(QCamera3HardwareInterface *parent) :
         mParent(parent) {}
